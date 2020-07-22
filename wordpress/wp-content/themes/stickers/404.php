5eaf8580dc23cb739069d12158bf486589baff82
Misraim Mendoza Arias <michmendar@gmail.com>
2020-07-22 14:31:08 -0500

    Update dtaabase and 404 page

---
+++ wordpress/wp-content/themes/stickers/404.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<!-- Content -->
+<section class="page">
+    <div class="page__container container">
+        <div class="page__row row">
+            <div class="page__column column">
+                <h2 class="page__title heading">Error 404</h2>
+            </div>
+        </div>
+        <div class="page__row row">
+            <div class="page__column column">
+                <div class="page__content heading heading--2">
+                    <?= __('Page not found', 'bls_theme'); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End content -->
+
+<?php get_footer(); ?>
